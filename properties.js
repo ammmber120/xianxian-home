07211057ad78c1ebf24b6d990067736702ebf005
Amber <kiss041520@gmail.com>
2026-06-08 23:51:18 +0800

    Update properties.js

--- properties.js
+++ properties.js
@@ -14,11 +14,18 @@ const properties = [
     location: "鼓山區｜近凹子底森林公園",
     price: "3,680 萬",
     tags: ["景觀視野", "雙平面車位", "永久棟距"],
-    cover: "https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?auto=format&fit=crop&w=1200&q=80",
+    cover: "images/A/01.jpg",
     images: [
-      "https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1600566753190-17f0baa2a6c3?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1600210492486-724fe5c67fb0?auto=format&fit=crop&w=1200&q=80"
+      "images/A/01.jpg",
+      "images/A/02.jpg",
+      "images/A/03.jpg",
+      "images/A/04.jpg",
+      "images/A/05.jpg",
+      "images/A/06.jpg",
+      "images/A/07.jpg",
+      "images/A/08.jpg",
+      "images/A/09.jpg",
+      "images/A/10.jpg"
     ],
     info: {
       坪數: "78.65坪",
@@ -44,11 +51,13 @@ const properties = [
     location: "鼓山區｜美術館生活圈",
     price: "1,988 萬",
     tags: ["質感裝潢", "邊間採光", "生活機能佳"],
-    cover: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1200&q=80",
+    cover: "images/B/01.jpg",
     images: [
-      "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1600566752355-35792bedcfea?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1600607688969-a5bfcd646154?auto=format&fit=crop&w=1200&q=80"
+      "images/B/01.jpg",
+      "images/B/02.jpg",
+      "images/B/03.jpg",
+      "images/B/04.jpg",
+      "images/B/05.jpg"
     ],
     info: {
       坪數: "45.32坪",
@@ -74,11 +83,11 @@ const properties = [
     location: "左營區｜近巨蛋捷運站",
     price: "1,598 萬",
     tags: ["近捷運站", "屋齡新", "精緻裝潢"],
-    cover: "https://images.unsplash.com/photo-1600607687644-c7171b42498b?auto=format&fit=crop&w=1200&q=80",
+   cover: "images/C/01.jpg",
     images: [
-      "https://images.unsplash.com/photo-1600607687644-c7171b42498b?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1600566753086-00f18fb6b3ea?auto=format&fit=crop&w=1200&q=80",
-      "https://images.unsplash.com/photo-1600607687920-4e2a09cf159d?auto=format&fit=crop&w=1200&q=80"
+      "images/A/01.jpg",
+      "images/A/02.jpg",
+      "images/A/03.jpg"
     ],
     info: {
       坪數: "32.14坪",
